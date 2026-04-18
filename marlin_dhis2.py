f9c8fa785214dd645effed94ca0e6df06e2dff58
godlisten-ngatara <gpn0021@gmail.com>
2026-04-18 15:43:25 +0300

    feat: Add GitHub Actions workflow for Docker build and push, update Dockerfile, and clean up README

--- marlin_dhis2.py
+++ marlin_dhis2.py
@@ -7,34 +7,37 @@ from dhis2_src.helper_functions import (
     load_models,
     preprocess_data,
     adjust_trailing_zero_prevalence,
-    generate_predictions_per_run
+    generate_predictions_per_run,
 )
 
 app = FastAPI(title="MARLIN Inference API")
 
-#Loading models
-WINDOW_SIZE = 10 #Don't change this
+# Loading models
+WINDOW_SIZE = 10  # Don't change this
 
-MODEL_EIR_PATH = "src/trained_model/causal/LSTM_EIR_4_layers_new_assymetric_9500runs_prevall.pth"
-MODEL_INC_PATH = "src/trained_model/causal/LSTM_Incidence_3_layers_15000run_causal_W10.pth"
-
-model_eir, model_inc, device = load_models(
-    MODEL_EIR_PATH,
-    MODEL_INC_PATH
+MODEL_EIR_PATH = (
+    "src/trained_model/causal/LSTM_EIR_4_layers_new_assymetric_9500runs_prevall.pth"
+)
+MODEL_INC_PATH = (
+    "src/trained_model/causal/LSTM_Incidence_3_layers_15000run_causal_W10.pth"
 )
 
-#Health Check
+model_eir, model_inc, device = load_models(MODEL_EIR_PATH, MODEL_INC_PATH)
+
+
+# Health Check
 @app.get("/")
 def health():
     return {"status": "MARLIN ready 🐟"}
 
-#Prediction Endpoint
+
+# Prediction Endpoint
 @app.post("/predict")
 async def predict(
     file: UploadFile = File(...),
-    run_column: str = "run", # This corresponds to a Geographic Unit, say Region, District...
+    run_column: str = "run",  # This corresponds to a Geographic Unit, say Region, District...
     time_column: str = "t",  # This corresponds to Time, say Month ...
-    prevalence_column: str = "prev_true" # The actual timeseries of prevalence
+    prevalence_column: str = "prev_true",  # The actual timeseries of prevalence
 ):
     """
     Upload a CSV or Parquet file containing prevalence data.
@@ -45,6 +48,8 @@ async def predict(
 
     # What i think is probable to Load file ---- Modify as necessary to take in JSON or other format
     try:
+        if file.filename is None:
+            raise HTTPException(status_code=400, detail="Filename is missing")
         if file.filename.endswith(".csv"):
             contents = await file.read()
             df = pd.read_csv(io.BytesIO(contents))
@@ -61,16 +66,14 @@ async def predict(
     if not required_cols.issubset(df.columns):
         raise HTTPException(
             status_code=400,
-            detail=f"Missing required columns: {required_cols - set(df.columns)}"
+            detail=f"Missing required columns: {required_cols - set(df.columns)}",
         )
 
     runs = df[run_column].unique().tolist()
 
     # Preprocess
     df = adjust_trailing_zero_prevalence(
-        df,
-        prevalence_column=prevalence_column,
-        seed=42
+        df, prevalence_column=prevalence_column, seed=42
     )
 
     df_scaled, has_true_values = preprocess_data(df)
@@ -80,27 +83,20 @@ async def predict(
 
     # Predicting
     run_results = generate_predictions_per_run(
-        df,
-        runs,
-        run_column,
-        WINDOW_SIZE,
-        model_eir,
-        model_inc,
-        device,
-        has_true_values
+        df, runs, run_column, WINDOW_SIZE, model_eir, model_inc, device, has_true_values
     )
 
-    #Output
+    # Output
     output = {}
 
     for run, result in run_results.items():
         output[run] = {
             "eir_pred": result["eir_preds_unscaled"][:, 0].tolist(),
-            "inc_pred": result["inc_preds_unscaled"][:, 0].tolist()
+            "inc_pred": result["inc_preds_unscaled"][:, 0].tolist(),
         }
 
     return {
         "n_runs": len(output),
         "runtime_seconds": round(time.time() - start_time, 3),
-        "predictions": output
+        "predictions": output,
     }
